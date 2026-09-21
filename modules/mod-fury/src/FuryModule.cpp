2babfdf010df2c219b5aeaaf65be2497bae979ca
Fury <81368760+FuryArchive@users.noreply.github.com>
2026-09-22 05:32:47 +0700

    chore: add mod-fury skeleton

---
+++ modules/mod-fury/src/FuryModule.cpp
@@ -0,0 +1,41 @@
+#include "ScriptMgr.h"
+
+#include "core/FuryApp.h"
+
+namespace
+{
+class FuryWorldScript final : public WorldScript
+{
+public:
+    FuryWorldScript()
+        : WorldScript(
+              "FuryWorldScript",
+              {
+                  WORLDHOOK_ON_STARTUP,
+                  WORLDHOOK_ON_UPDATE,
+                  WORLDHOOK_ON_SHUTDOWN
+              })
+    {
+    }
+
+    void OnStartup() override
+    {
+        Fury::App::Instance().Initialize();
+    }
+
+    void OnUpdate(uint32 diff) override
+    {
+        Fury::App::Instance().Update(diff);
+    }
+
+    void OnShutdown() override
+    {
+        Fury::App::Instance().Shutdown();
+    }
+};
+}
+
+void AddFuryScripts()
+{
+    new FuryWorldScript();
+}
