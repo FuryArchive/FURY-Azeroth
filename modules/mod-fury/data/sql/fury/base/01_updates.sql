497493d6389967a09d93cb573ce3a6d069306f84
Fury <81368760+FuryArchive@users.noreply.github.com>
2026-09-22 06:13:31 +0700

    fix(db): remove prefixed updater base filename

--- modules/mod-fury/data/sql/fury/base/01_updates.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `updates` (
-  `name` varchar(200) NOT NULL COMMENT 'filename with extension of the update',
-  `hash` char(40) DEFAULT '' COMMENT 'sha1 hash of the sql file',
-  `state` enum('RELEASED','ARCHIVED','CUSTOM') NOT NULL DEFAULT 'RELEASED',
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `speed` int unsigned NOT NULL DEFAULT 0,
-  PRIMARY KEY (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC
-  COMMENT='List of all applied updates in this database';
