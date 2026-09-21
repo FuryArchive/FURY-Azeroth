13d24887c4ccd079afcebffaf3ea22c4bd9dcaef
Fury <81368760+FuryArchive@users.noreply.github.com>
2026-09-22 06:06:53 +0700

    feat(db): add module-owned acore_fury database

---
+++ modules/mod-fury/src/database/FuryDatabase.h
@@ -0,0 +1,68 @@
+#ifndef MOD_FURY_DATABASE_H
+#define MOD_FURY_DATABASE_H
+
+#include "DatabaseEnvFwd.h"
+#include "ModuleDatabasePool.h"
+#include "MySQLConnection.h"
+#include "PreparedStatement.h"
+#include "Transaction.h"
+
+#include <memory>
+
+namespace Fury
+{
+enum DatabaseStatements : uint32
+{
+    MAX_FURY_DATABASE_STATEMENTS
+};
+
+class DatabaseConnection final : public MySQLConnection
+{
+public:
+    using Statements = DatabaseStatements;
+
+    explicit DatabaseConnection(MySQLConnectionInfo& connInfo);
+    ~DatabaseConnection() override = default;
+
+    void DoPrepareStatements() override;
+};
+
+using DatabasePreparedStatement = PreparedStatement<DatabaseConnection>;
+using DatabaseTransaction = std::shared_ptr<Transaction<DatabaseConnection>>;
+
+class DatabasePool final : public ModuleDatabasePool
+{
+public:
+    DatabasePreparedStatement* GetPreparedStatement(DatabaseStatements index)
+    {
+        return new DatabasePreparedStatement(index, GetPreparedStatementParamCount(index));
+    }
+
+    using ModuleDatabasePool::Execute;
+    using ModuleDatabasePool::Query;
+
+    DatabaseTransaction BeginTransaction()
+    {
+        return std::make_shared<Transaction<DatabaseConnection>>();
+    }
+
+    void CommitTransaction(DatabaseTransaction transaction)
+    {
+        DirectCommitTransaction(transaction);
+    }
+
+    void WarnAboutSyncQueries([[maybe_unused]] bool apply) {}
+
+    [[nodiscard]] std::size_t QueueSize() const { return 0; }
+
+protected:
+    MySQLConnection* CreateConnection(MySQLConnectionInfo& connInfo) override
+    {
+        return new DatabaseConnection(connInfo);
+    }
+};
+
+extern DatabasePool FuryDatabase;
+}
+
+#endif
