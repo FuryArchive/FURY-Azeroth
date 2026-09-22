0cb057a4852bb74e7f5ff78410b6199bec963e54
Fury <81368760+FuryArchive@users.noreply.github.com>
2026-09-23 04:32:37 +0700

    feat(director): compose Director services

--- modules/mod-fury/src/core/FuryApp.cpp
+++ modules/mod-fury/src/core/FuryApp.cpp
@@ -28,7 +28,9 @@ App::App()
       _diagnostics(*this, _diagnosticsRepository),
       _campaign(_campaignRepository, _events),
       _proofs(_proofRepository, _events),
-      _contracts(_contractRepository, _events)
+      _contracts(_contractRepository, _events),
+      _director(_directorRepository, _events),
+      _directorReconciliation(_directorRepository)
 {
     _eventBus.RegisterConsumer(_chronicle);
     _eventBus.RegisterConsumer(_contracts);
