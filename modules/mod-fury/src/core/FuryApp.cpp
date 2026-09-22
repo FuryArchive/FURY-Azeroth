5b1950f197f8e00be2f7d15da4de3bc5e1b415ab
Fury <81368760+FuryArchive@users.noreply.github.com>
2026-09-22 09:19:59 +0700

    feat(professions): register profession order replay consumer

--- modules/mod-fury/src/core/FuryApp.cpp
+++ modules/mod-fury/src/core/FuryApp.cpp
@@ -31,11 +31,13 @@ App::App()
       _contracts(_contractRepository, _events),
       _contractRewards(_contractRepository, _campaign, _rewards),
       _director(_directorRepository, _events),
-      _directorReconciliation(_directorRepository)
+      _directorReconciliation(_directorRepository),
+      _professionOrders(_professionOrderRepository, _events)
 {
     _eventBus.RegisterConsumer(_chronicle);
     _eventBus.RegisterConsumer(_contracts);
     _eventBus.RegisterConsumer(_contractRewards);
+    _eventBus.RegisterConsumer(_professionOrders);
 }
 
 App& App::Instance()
