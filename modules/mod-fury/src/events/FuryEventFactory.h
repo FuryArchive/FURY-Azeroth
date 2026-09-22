c3a8f40859e6ab882b0b0a4b7599900bf4256e73
Fury <81368760+FuryArchive@users.noreply.github.com>
2026-09-22 09:20:13 +0700

    feat(events): add profession craft event factory

--- modules/mod-fury/src/events/FuryEventFactory.h
+++ modules/mod-fury/src/events/FuryEventFactory.h
@@ -26,6 +26,11 @@ public:
         uint32 count,
         ObjectGuid lootGuid);
     static FuryEvent ItemCreated(Player* player, Item* item, uint32 count);
+    static FuryEvent ProfessionCrafted(
+        Player* player,
+        uint32 skillId,
+        uint32 recipeSpellId,
+        uint32 itemId);
 
 private:
     static FuryEvent Base(Player* player, std::string type);
