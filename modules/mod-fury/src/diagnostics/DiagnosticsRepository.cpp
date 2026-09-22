5610df888b3dd92bb6b20b939dc4b2d719dddf12
Fury <81368760+FuryArchive@users.noreply.github.com>
2026-09-22 07:03:15 +0700

    feat(commands): add FURY diagnostics service

---
+++ modules/mod-fury/src/diagnostics/DiagnosticsRepository.cpp
@@ -0,0 +1,26 @@
+#include "DiagnosticsRepository.h"
+
+#include "database/FuryDatabase.h"
+#include "DatabaseEnv.h"
+
+namespace Fury
+{
+std::optional<KernelSnapshot> DiagnosticsRepository::Snapshot() const
+{
+    PreparedQueryResult result =
+        FuryDatabase.Query(FuryDatabase.GetPreparedStatement(FURY_SEL_DIAGNOSTIC_COUNTS));
+    if (!result)
+        return std::nullopt;
+
+    Field* fields = result->Fetch();
+
+    KernelSnapshot snapshot;
+    snapshot.households = fields[0].Get<uint64>();
+    snapshot.householdMembers = fields[1].Get<uint64>();
+    snapshot.events = fields[2].Get<uint64>();
+    snapshot.consumers = fields[3].Get<uint64>();
+    snapshot.rewardClaims = fields[4].Get<uint64>();
+    snapshot.chronicleEntries = fields[5].Get<uint64>();
+    return snapshot;
+}
+}
